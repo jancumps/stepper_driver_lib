a4b139cb1ef3fd74133771f2e4e671ee86a00500
Jan Cumps <jcumps@gmail.com>
2025-04-26 11:59:25 +0200

    typo

--- source/stepper_driver.cpp
+++ source/stepper_driver.cpp
@@ -7,7 +7,7 @@ class stepper_driver { // driver out of sleep as long as object in scope
 public:
     virtual bool init() = 0;
     // configure microsteps / step. return false if not supported
-    virtual bool microsteps(unisgned int microsteps) = 0;
+    virtual bool microsteps(unsigned int microsteps) = 0;
     virtual void enable(bool enable) = 0;
 };
 
