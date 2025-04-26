7dde9444c73dd40d7a0de2b3dcc323354f59d950
Jan Cumps <jcumps@gmail.com>
2025-04-26 11:11:51 +0200

    abstract base class

---
+++ source/stepper_driver.cpp
@@ -0,0 +1,14 @@
+module;
+
+export module stepper_driver;
+export namespace stepper_driver {
+
+class stepper_driver { // driver out of sleep as long as object in scope
+public:
+    virtual bool init() = 0;
+    // configure microsteps / step. return false if not supported
+    virtual bool microsteps(uint microsteps) = 0;
+    virtual void enable(bool enable) = 0;
+};
+
+} // namespace stepper_driver
