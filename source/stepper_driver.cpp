db8ff93001684cf2e74a870a95033684054ec091
Jan Cumps <jcumps@gmail.com>
2025-04-26 12:56:09 +0200

    promote waekeup to abstract level

--- source/stepper_driver.cpp
+++ source/stepper_driver.cpp
@@ -11,4 +11,13 @@ public:
     virtual void enable(bool enable) = 0;
 };
 
+class wakeup { // driver out of sleep as long as object in scope
+    public:    
+        wakeup(stepper_driver& driver) : driver_(driver) { driver.enable(true); }
+        ~wakeup() { driver_.enable(false); }
+    private:
+        stepper_driver& driver_;
+    };
+   
+
 } // namespace stepper_driver
